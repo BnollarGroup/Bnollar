e4b21f4b45629a570bb4898ceade97190a883dcf
levaniakobidze <levaniakobidze25@gmail.com>
2023-02-02 22:52:47 +0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import Landing from "./pages/landing/Landing";
 import SignIn from "./pages/SignIn/SignIn";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Landing/> */}
-      <SignIn />
-      {/* <h1>Hello People</h1> */}
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
     </div>
   );
 };
