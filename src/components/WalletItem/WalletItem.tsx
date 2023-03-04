c236c6171524408eadee107b3b17f07cd1ae88f3
Giorgi <drgls@mail.ru>
2023-03-05 02:15:10 +0400

    WalletItem component created

---
+++ src/components/WalletItem/WalletItem.tsx
@@ -0,0 +1,28 @@
+import classes from './WalletItem.module.css'
+
+interface WalletItemProps {
+  image?: string
+  title: string
+  callback?: () => void
+}
+const WalletItem = ({ image, title, callback }: WalletItemProps) => {
+  const handleClick = () => {
+    if (callback) {
+      callback()
+    }
+  }
+
+  return (
+    <button onClick={handleClick} className={classes.wallet_item}>
+      {image ? <img src={image} alt="wallet" /> : null}
+      <p
+        className={classes.wallet_name}
+        style={!image ? { opacity: '40%' } : {}}
+      >
+        {title}
+      </p>
+    </button>
+  )
+}
+
+export default WalletItem
