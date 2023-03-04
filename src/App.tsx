847aeede88b49298c125d3320979091c98e48b32
Giorgi <drgls@mail.ru>
2023-03-04 16:00:55 +0400

    landing page updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import "./App.css";
-import Landing from "./pages/landing/Landing";
-import SignIn from "./pages/SignIn/SignIn";
-import { Routes, Route } from "react-router-dom";
-import Register from "./pages/register/Register";
-import UploadNft from "./pages/uploadNft/UploadNft";
-import Home from "./pages/Home/Home";
-import Post from "./pages/Post/Post";
-import Nft from "./pages/Nft/Nft";
+import React from 'react'
+import './App.css'
+import Landing from './pages/landing/Landing'
+import SignIn from './pages/SignIn/SignIn'
+import { Routes, Route } from 'react-router-dom'
+import Register from './pages/register/Register'
+import UploadNft from './pages/uploadNft/UploadNft'
+import Home from './pages/Home/Home'
+import Post from './pages/Post/Post'
+import Nft from './pages/Nft/Nft'
 
 const App = () => {
   return (
-    <div className="App" >
+    <div className="App">
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/connect-wallet" element={<SignIn />} />
         <Route path="/register" element={<Register />} />
         <Route path="/upload-nft" element={<UploadNft />} />
         <Route path="/home" element={<Home />} />
@@ -22,7 +22,7 @@ const App = () => {
         <Route path="/nft" element={<Nft />} />
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,19 +1,20 @@
-import React, { FC } from "react";
-import styles from "./Landing.module.css";
-import bnollarLogo from "../../resources/images/logo/bnollar_logo.svg";
-import frame from "../../resources/images/landing/frame.svg";
-import icon1 from "../../resources/images/landing/icon1.svg";
-import icon2 from "../../resources/images/landing/icon2.svg";
-import icon6 from "../../resources/images/landing/icon6.svg";
+import React, { FC } from 'react'
+import styles from './Landing.module.css'
+import bnollarLogo from '../../resources/images/logo/bnollar_logo.svg'
+import frame from '../../resources/images/landing/frame.svg'
+import icon1 from '../../resources/images/landing/icon1.svg'
+import icon2 from '../../resources/images/landing/icon2.svg'
+import icon6 from '../../resources/images/landing/icon6.svg'
+import { Link } from 'react-router-dom'
 const Landing: FC = () => {
-  console.log(styles);
+  console.log(styles)
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
         <img src={bnollarLogo} alt="bnollar" />
-        <button className={styles.getStarted}>
-          <p className={styles.getStartedText}>Get started</p>
-        </button>
+        <Link to={'/connect-wallet'}>
+          <button className={styles.getStarted}>Get started</button>
+        </Link>
       </div>
       <div className={styles.welcomeWrapper}>
         <div className={styles.welcome}>
@@ -22,27 +23,27 @@ const Landing: FC = () => {
             Join us on our journey to a more open and decentralized internet.
             Sign up now and experience the future of social media.
           </div>
-          <div className={styles.welcomeGetStarted}>
-            <button>Get started</button>
-          </div>
+          <Link to={'/connect-wallet'}>
+            <button className={styles.welcomeGetStarted}>Get started</button>
+          </Link>
         </div>
       </div>
       <div className={styles.footerWrapper}>
         <div className={styles.left}>
-            <div><img src={icon6} alt='nft' /></div>
-            <div><img src={icon1} alt='nft'/></div>
-            <div><img src={icon2} alt='nft'/></div>
+          <img className={styles.landingIcon} src={icon6} alt="nft" />
+          <img className={styles.landingIcon} src={icon1} alt="nft" />
+          <img className={styles.landingIcon} src={icon2} alt="nft" />
         </div>
         <div className={styles.center}>
           <img src={frame} />
         </div>
         <div className={styles.right}>
-            <div><img src={icon6} alt='nft'/></div>
-            <div><img src={icon1} alt='nft'/></div>
-            <div><img src={icon2} alt='nft'/></div>
+          <img className={styles.landingIcon} src={icon6} alt="nft" />
+          <img className={styles.landingIcon} src={icon1} alt="nft" />
+          <img className={styles.landingIcon} src={icon2} alt="nft" />
         </div>
       </div>
     </div>
-  );
-};
-export default Landing;
+  )
+}
+export default Landing
