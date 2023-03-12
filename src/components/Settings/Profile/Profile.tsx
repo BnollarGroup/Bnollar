4176253596f7ee88a3be94aea41ff169353f73bb
Giorgi <drgls@mail.ru>
2023-03-12 18:06:29 +0400

    profile settings component implemented

--- src/components/Settings/Profile/Profile.tsx
+++ src/components/Settings/Profile/Profile.tsx
@@ -1,5 +1,120 @@
+import { useState } from 'react'
+import contStyles from '../SettingsContainer.module.css'
+import styles from './Profile.module.css'
+
+interface formState {
+  username: string
+  displayName: string
+  email: string
+  websiteURL: string
+  twitter: string
+  instagram: string
+}
+
 const Profile = () => {
-  return <div>Profile</div>
+  const initialFormState = {
+    username: '',
+    displayName: '',
+    email: '',
+    websiteURL: '',
+    twitter: '',
+    instagram: '',
+  }
+
+  const [formState, setFormState] = useState<formState>({ ...initialFormState })
+
+  const changeHandler = (property: string, value: string) => {
+    const updatedState = { ...formState, [property]: value }
+    setFormState(updatedState)
+  }
+  return (
+    <div className={contStyles.containerWrapper}>
+      <form className={contStyles.settingsContainer}>
+        <div className={styles.profileDetails}>
+          <h2 className={contStyles.contHeader}>Profile</h2>
+          <p className={contStyles.message}>Edit your profile details</p>
+          <div className={contStyles.line} />
+          <div className={styles.inputsFlex}>
+            <div className={styles.inputWrapper}>
+              <p className={contStyles.message}>Username</p>
+              <input
+                className={styles.input}
+                onChange={(e) => changeHandler('username', e.target.value)}
+                type="text"
+                placeholder="Username..."
+                value={formState.username}
+              />
+            </div>
+            <div className={styles.inputWrapper}>
+              <p className={contStyles.message}>Display name</p>
+              <input
+                className={styles.input}
+                onChange={(e) => changeHandler('displayName', e.target.value)}
+                type="text"
+                value={formState.displayName}
+                placeholder="Display name..."
+              />
+            </div>
+          </div>
+          <div className={styles.inputWrapper}>
+            <p className={contStyles.message}>Email address</p>
+            <input
+              className={styles.input}
+              onChange={(e) => changeHandler('email', e.target.value)}
+              type="email"
+              value={formState.email}
+              placeholder="example@gmail.com"
+            />
+          </div>
+        </div>
+        <div>
+          <h2 className={contStyles.contHeader}>Social link</h2>
+          <p className={contStyles.message}>
+            Add your existing social links to build a stronger reputatior
+          </p>
+          <div className={contStyles.line} />
+          <div className={styles.inputWrapper}>
+            <p className={contStyles.message}>Website URL</p>
+            <input
+              className={styles.input}
+              onChange={(e) => changeHandler('websiteURL', e.target.value)}
+              type="text"
+              value={formState.websiteURL}
+              placeholder="https://"
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <p className={contStyles.message}>Twitter</p>
+            <input
+              className={styles.input}
+              onChange={(e) => changeHandler('twitter', e.target.value)}
+              type="text"
+              value={formState.twitter}
+              placeholder="Twitter username"
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <p className={contStyles.message}>Instagram</p>
+            <input
+              className={styles.input}
+              onChange={(e) => changeHandler('instagram', e.target.value)}
+              type="text"
+              value={formState.instagram}
+              placeholder="Instagram username"
+            />
+          </div>
+        </div>
+        <div className={contStyles.buttonsWrapper}>
+          <button type="button" className={contStyles.cancelButton}>
+            Cancel
+          </button>
+          <button type="button" className={contStyles.updateButton}>
+            Update profile
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Profile
