3a7a3f774eb2a02d644b080296e142e7eed52d42
Alexander Grigoryan <a.grigoryan@inbox.ru>
2023-09-07 15:12:41 +0400

    delete some commented code

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -44,40 +44,4 @@ function Navbar() {
 
 export default Navbar;
 
-//       <div className={classes.navbar_container}>
-//         <div className={classes.left_cont}>
-//           <div className={classes.logo_cont}>
-//             <img src={Logo} alt="Logo" />
-//           </div>
-//           <div className={classes.search_input_cont}>
-//             <img src={Search} alt="search" />
-//             <input type="text" placeholder="Search" />
-//           </div>
-//         </div>
-//         <div className={classes.right_cont}>
-//           <div className={classes.create}>
-//             <span>+</span>
-//             Create
-//           </div>
-//           <div className={classes.notifications}>
-//             <img src={Notifications} alt="notifications" />
-//           </div>
-//           <div className={classes.messanger}>
-//             <img src={Messanger} alt="messanger" />
-//           </div>
-//           <div className={classes.avatar}>
-//             <img
-//               src={
-//                 'https://www.elitesingles.co.uk/wp-content/uploads/sites/59/2019/11/2b_en_articleslide_sm2-350x264.jpg'
-//               }
-//               alt="avatar"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
 
--- src/pages/uploadNft/UploadNft.tsx
+++ src/pages/uploadNft/UploadNft.tsx
@@ -133,16 +133,6 @@ function UploadNft() {
         </div>
       </div>
       </section>
-// {/* 
-// import React from "react";
-// import Navbar from "../../components/Navbar/Navbar";
-// import classes from "./UploadNft.module.css";
-
-// function UploadNft() {
-//   return (
-//     <section className={classes.upload_nft}>
-//       <Navbar />
-//     </section> */}
   );
 }
 export default UploadNft;
