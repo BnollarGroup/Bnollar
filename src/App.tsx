b51c0fa08073da58f52d55ae41f7b880c3f6268e
Nika Shamiladze <shamgoingzuck@gmail.com>
2023-10-14 17:32:09 +0400

    fix: suspense problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { publicRoutes } from "routes/route-generator";
 
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        {publicRoutes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<route.component />}
-          />
-        ))}
-      </Routes>
+      <Suspense fallback={false}>
+        <Routes>
+          {publicRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+      </Suspense>
     </div>
   );
 };
