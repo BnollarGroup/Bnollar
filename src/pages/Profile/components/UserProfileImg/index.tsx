9f44e3df2d5050711df3a46d8beb7e0b6f075128
ninatsiklauri <tsiklaurinino88@gmail.com>
2023-10-28 03:21:35 +0400

    style: change markup on profile page

--- src/pages/Profile/components/UserProfileImg/index.tsx
+++ src/pages/Profile/components/UserProfileImg/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./UserProfileImg.module.css"
 import UserIcon from "lib/resources/images/icons/user-icon.png";
+import editImg from "lib/resources/images/Frame 4.png"
 
 
 function index() {
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleClick = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
-      <div className={style.userimg}>
-            <img src={UserIcon} />
-      </div>
+    <div className={style.userimg}>
+      <img src={UserIcon} className={style.profileImg} />
+      <img src={editImg} alt="" className={style.editImg} onClick={handleClick} />
+      {openModal && (
+        <div className={style.openProfileditdrop}>
+          <button className={style.dropProfilebtn}>Upload Photo</button>
+          <button className={style.dropProfilebtn}>Remove</button>
+        </div>
+      )}
+    </div>
   )
 }
 
