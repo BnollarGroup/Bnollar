7ed18b8efa084c20794f33a7880bfea7bc0ec6cf
Nika Shamiladze <shamgoingzuck@gmail.com>
2023-11-02 17:10:36 +0400

    Update index.tsx

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,38 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "components/Navbar";
 import style from "./Profile.module.css";
-import UserIcon from "lib/resources/images/icons/user-icon.png";
-import Verifed from "lib/resources/images/icons/twitter-verified-badge.png";
 import Grid from "lib/resources/images/icons/view-grid.png";
-import PostImg from "lib/resources/images/icons/post-img.png";
-import Edit from "lib/resources/images/icons/edit (2).png";
 import StatsReport from "lib/resources/images/icons/stats-report.png";
 import LeftSideBar from "components/Leftsidebar/LeftSideBar";
 import Layout from "providers/Layout";
 import Tabs from "components/Tabs";
-import horiz from "lib/resources/images/profile-chat/more-horiz.png";
-// import ProfileEditDetailsModal from "providers/Modals/Profile/EditDetails";
-import FeedHeader from "components/Home/FeedHeader";
-import UserInformation from "./components/UserInformation"
-import Cover from "pages/Profile/components/ProfileCover";
-// import EditCoverImg from "./components/User/component/ProfileCover/EditCoverImg"
-import RighSideBar from "pages/Home/RightSideBar/RighSideBar";
-import AboutProfile from "pages/Profile/components/aboutProfile";
-import User from "./components/User"
-import Post from "./components/Post"
-import { dataBase } from "./components/Post/data";
+import UserInformation from "./components/UserInformation";
+import User from "./components/User";
+import Post from "./components/Post";
+import { dataBase as data } from "./components/Post/data";
 import { DataType } from "./components/Post/types.d";
-import WritePost from "components/Home/FeedHeader"
+import WritePost from "components/Home/FeedHeader";
 import MobileNavBar from "components/MobileNavBar";
 
 export type Page = "feed" | "NFTs" | "media";
 
 function Profile() {
-
-  const [openCover, setOperCover] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [data] = useState<DataType[]>(dataBase);
-
   const pages: { page: Page; icon: any }[] = [
     {
       page: "feed",
@@ -44,36 +28,6 @@ function Profile() {
     },
     { page: "media", icon: StatsReport },
   ];
-
-  let editcover;
-  if (openCover) {
-    editcover = (
-      <div className={style.opencovereditdrop}>
-        <button className={style.dropcoverbtn}>Upload Photo</button>
-        <button className={style.dropcoverbtn}>Reposition</button>
-        <button className={style.dropcoverbtn}>Remove</button>
-      </div>
-    );
-  }
-
-  function openCoverEdit() {
-    setOperCover(true);
-  }
-
-  const [openProfilePic, setProfilePic] = useState(false);
-  let profilePicEdit;
-  if (openProfilePic) {
-    profilePicEdit = (
-      <div className={style.profilepicedit}>
-        <button className={style.dropcoverbtn}>Upload Photo</button>
-        <button className={style.dropcoverbtn}>Remove</button>
-      </div>
-    );
-  }
-
-  function openProfile() {
-    setProfilePic(true);
-  }
 
   const [currentPage, setCurrentPage] = useState<Page>("feed");
   return (
@@ -87,8 +41,10 @@ function Profile() {
           <div className={style.LeftSideBar}>
             <LeftSideBar />
           </div>
+
           <div className={style.userContainer}>
             <User />
+
             <Tabs
               pages={pages}
               currentPage={currentPage}
@@ -97,12 +53,13 @@ function Profile() {
 
             <div className={style.gap}>
               <div className={style.tabsLine}></div>
+
               <div className={style.postContainer}>
                 <div className={style.posts}>
                   <div className={style.writePost}>
-                  <WritePost />
-
+                    <WritePost />
                   </div>
+
                   {data.map((dataItem: DataType, index: number) => {
                     return (
                       <div key={index}>
@@ -111,11 +68,13 @@ function Profile() {
                     );
                   })}
                 </div>
+
                 <div className={style.profileInformation}>
                   <UserInformation />
                 </div>
               </div>
             </div>
+
             <div className={style.MobileNavBar}>
               <MobileNavBar />
             </div>
@@ -123,7 +82,6 @@ function Profile() {
         </div>
       </Layout>
     </div>
-
   );
 }
 
