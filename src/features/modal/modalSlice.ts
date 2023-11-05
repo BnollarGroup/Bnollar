a02257a0c70b420f104f53d8f8e50cace93e53ca
Nika Shamiladze <shamgoingzuck@gmail.com>
2023-11-05 19:51:26 +0400

    ...

--- src/features/modal/modalSlice.ts
+++ src/features/modal/modalSlice.ts
@@ -15,6 +15,8 @@ export const modalSlice = createSlice({
   initialState,
   reducers: {
     change: (state, action: PayloadAction<Modal>) => {
+      window.scrollTo(0, 0);
+
       state.modal = action.payload;
     },
 
