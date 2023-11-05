448ac3846d1f27a1f6044d7b63f0da279164dcad
Nika Shamiladze <shamgoingzuck@gmail.com>
2023-11-05 11:18:59 +0400

    feat: navbar/sticking

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import classes from "./Navbar.module.css";
+import style from "./Navbar.module.css";
 import Logo from "lib/resources/images/logo/bnollar_logo.svg";
 import Plus from "lib/resources/images/icons/plus.svg";
 import Search from "lib/resources/images/Navbar/search.svg";
@@ -8,31 +8,31 @@ import AvatarImg from "lib/resources/images/Navbar/avatar.png";
 
 function Navbar() {
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.left_cont}>
-        <div className={classes.logo_cont}>
+    <nav className={style.navbar}>
+      <div className={style.left_cont}>
+        <div className={style.logo_cont}>
           <img src={Logo} alt="Logo" />
         </div>
-        <div className={classes.search_input_cont}>
+        <div className={style.search_input_cont}>
           <img src={Search} alt="search" />
           <input type="text" placeholder="Search" />
         </div>
       </div>
 
-      <div className={classes.right_cont}>
-        <div className={classes.create}>
+      <div className={style.right_cont}>
+        <div className={style.create}>
           <img src={Plus} alt="Plus" />
           <p>Create</p>
         </div>
-        <div className={classes.notifications}>
+        <div className={style.notifications}>
           <img src={Notifications} alt="notifications" />
           <img src={Plus} alt="notifications" />
         </div>
-        <div className={classes.messenger}>
+        <div className={style.messenger}>
           <img src={Messanger} alt="messenger" />
           <img src={Search} alt="messenger" />
         </div>
-        <div className={classes.avatar}>
+        <div className={style.avatar}>
           <img src={AvatarImg} alt="avatar" />
         </div>
       </div>
