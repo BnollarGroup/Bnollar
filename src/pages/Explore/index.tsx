e90a73fc970c5e97beb8615565e33d84c4562721
Nika Shamiladze <shamgoingzuck@gmail.com>
2023-11-06 21:10:00 +0400

    Update index.tsx

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -3,7 +3,7 @@ import Layout from "providers/Layout";
 import Navbar from "components/Navbar";
 import style from "./Explore.module.css";
 import LeftSidebar from "components/LeftSidebar";
-import RightSidebar from "./components/RightSideBar";
+import RightSidebar from "./components/RightSidebar";
 import MobileNavBar from "components/MobileNavbar";
 import CategoryButtons from "./components/CategoryButtons";
 import Post from "components/Post";
